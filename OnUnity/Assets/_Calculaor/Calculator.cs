3fd8946d97d63e5c3de50825f3b4ebd2f5f4828d
Роман Мироненко <79697765+Roman784@users.noreply.github.com>
2025-12-07 14:58:35 +0300

    .

---
+++ OnUnity/Assets/_Calculaor/Calculator.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+namespace Calculator
+{
+    public class Calculator : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _result;
+        [SerializeField] private TMP_Text _preview;
+
+        public void AddNumber(int number)
+        {
+
+        }
+
+        public void Sum()
+        {
+
+        }
+
+        public void Divide()
+        {
+
+        }
+    }
+}
